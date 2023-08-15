0977de2368a4eb11d65cdf234084add880dd9453
Nicolas Maltais <nic.maltais@gmail.com>
2023-08-15 01:08:33 +0100

    Initial commit audio output test

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Albert
 Virtual Assistant
+
+
+Installation
+
+1- connect to wifi
+
+2- enable ssh
+
+3- speaker-test -c2
+
+4- sudo apt-get install python-pyaudio
+
+5- sudo apt-get install python3-venv -y
+
+6- create venv and install reqs
+    python -m venv albertvenv
+    source albertvenv/bin/activate (on Unix-like systems)
+    venv_name\Scripts\activate (on Windows)
+    pip install -r requirements.txt
+
+7- run the program
+   python src/main.py audio/yes.py
---
+++ src/main.py
@@ -0,0 +1,45 @@
+import os
+import sys
+import time
+import wave
+import pyaudio
+
+def play_audio(audio_file):
+    chunk = 1024
+
+    try:
+        wf = wave.open(audio_file, 'rb')
+        p = pyaudio.PyAudio()
+
+        stream = p.open(
+            format=p.get_format_from_width(wf.getsampwidth()),
+            channels=wf.getnchannels(),
+            rate=wf.getframerate(),
+            output=True
+        )
+
+        data = wf.readframes(chunk)
+
+        while data:
+            stream.write(data)
+            data = wf.readframes(chunk)
+
+        stream.stop_stream()
+        stream.close()
+
+        p.terminate()
+
+    except Exception as e:
+        print(f"Error playing audio: {e}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python main.py <audio_file>")
+        sys.exit(1)
+
+    audio_file = sys.argv[1]
+    if not os.path.exists(audio_file):
+        print("Audio file not found.")
+        sys.exit(1)
+
+    play_audio(audio_file)
